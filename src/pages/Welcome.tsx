16ee5c7a704f6bd51244a1f7df3eb35898c33e4b
Edson Nascimento <edsnascimento89@gmail.com>
2022-05-31 18:02:33 -0300

    [working icons/fonts]

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,53 +1,86 @@
-import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, StatusBar, Platform } from 'react-native';
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Platform,
+  TouchableOpacity,
+  Dimensions
+} from "react-native";
+import { Feather } from '@expo/vector-icons';
 
-import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button';
-import colors from '../styles/colors';
+import wateringImg from "../assets/watering.png";
+import colors from "../styles/colors";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export function Welcome(){
-    return(
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas de {'\n'}
-                forma fácil
-            </Text>
+export function Welcome() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {"\n"}
+        suas plantas de {"\n"}
+        forma fácil
+      </Text>
 
-            <Image style={styles.image} source={wateringImg}/>
+      <Image 
+        style={styles.image} 
+        source={wateringImg}
+        resizeMode="contain" 
+        />
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar sua Plantas.
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar sua Plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
 
-            <Button title='>'/>
-        </SafeAreaView>
-    )
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text> 
+            <Feather
+                name="chevron-right"
+                style={styles.buttonIcon} 
+            />
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: colors.heading,
-        marginTop: 38
-    },
-    subtitle: {
-        textAlign: 'center',
-        fontSize: 18,
-        paddingHorizontal: 20,
-        color: colors.heading
-    },
-    image: {
-        width: 292,
-        height: 284
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
+  }
 });
